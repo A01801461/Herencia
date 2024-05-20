335b2e3f25b49c8451e125071804c9fe463c5a0d
A01801461 <149410171+A01801461@users.noreply.github.com>
2024-05-20 13:42:04 +0000

    herencia

--- main.cpp
+++ main.cpp
@@ -1,1 +1,37 @@
+// librerias
+#include <iostream>
+using std::cout;
+using std::endl;
 
+// clase Padre (Super clase)
+class Padre
+{
+    public:
+    int x = 5;
+
+    void imprimir()
+    {
+        cout << "El valor de x: " << x << endl;
+    }
+};
+
+// clase hija (Sub clase)
+class Hija : Padre // hereda todo de la clase padre
+{
+    public:
+    void imprimir()
+    {
+        cout << "El valor de x: " << x << endl;
+    }
+};
+
+// Funcion main
+int main()
+{
+    Hija h;
+    h.imprimir();
+    //Padre p;
+    //p.x = 10;
+    //p.imprimir();
+    return 0;
+};
